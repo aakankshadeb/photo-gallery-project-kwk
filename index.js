a40f97978b401d052e57e19aa0b0f17aeff114a6
Apple <apple@Apples-MacBook-Pro.local>
2019-07-25 09:37:04 -0700

    full photo gallery project

---
+++ index.js
@@ -0,0 +1,14 @@
+/*var string = "megan";
+var stringLength = string.length % 2;
+if (stringLength === 0){
+  console.log("even");
+}else{
+  console.log("odd");
+}
+prompt("who is the queen");
+var queen = console.input;
+if (queen == "beyonce") {
+  console.log("youre right")
+} else {
+  console.log("youre wrong")
+} */
