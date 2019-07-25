9b5b27eb6305661ca07c158c3bffc6681e1b22dc
Apple <apple@Apples-MacBook-Pro.local>
2019-07-25 16:12:03 -0700

    final project

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ if (stringLength === 0){
   console.log("even");
 }else{
   console.log("odd");
-}
-prompt("who is the queen");
+}*/
+
+/*prompt("who is the queen");
 var queen = console.input;
 if (queen == "beyonce") {
   console.log("youre right")
 } else {
   console.log("youre wrong")
-} */
+}
+*/
+
+/*prompt ("Hi");
+var greeting = console.input;
+if (greeting == "Hello") {
+  console.log("No")
+} else {
+  console.log("Yes")
+}*/
+
+/*var queen = prompt("who is the queen");
+if (queen == "beyonce") {
+  console.log("youre right")
+} else {
+  console.log("youre wrong")
+}*/
